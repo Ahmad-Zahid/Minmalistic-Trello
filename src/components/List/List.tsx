b1685d5ea6b0e2630e4884c28087f509b0b49cf9
AhmadZahid <ahmad.zahid2000@gmail.com>
2021-06-06 17:44:55 +0500

    lazy loading

--- src/components/List/List.tsx
+++ src/components/List/List.tsx
@@ -1,15 +1,15 @@
 // Packages
-import { ReactElement } from "react";
+import { ReactElement, lazy, Suspense } from "react";
 import { Paper, CssBaseline } from "@material-ui/core";
 import { Droppable, Draggable } from "react-beautiful-dnd";
 
 // Components
-import Title from "./Title";
-import Card from "../Card";
-import InputContainer from "../Input/InputContainer";
+const Title = lazy(() => import("./Title"));
+const Card = lazy(() => import("../Card"));
+const InputContainer = lazy(() => import("../Input/InputContainer"));
 
 // Stylesheet
-import {useListStyle} from './styles'
+import { useListStyle } from "./styles";
 
 interface ListProps {
   list: {
@@ -22,13 +22,17 @@ interface ListProps {
 export default function List({ list, index }: ListProps): ReactElement {
   const classes = useListStyle();
   const { title, id, cards } = list;
+
+  const loading = () => <div>Loading...</div>;
   return (
     <Draggable draggableId={id} index={index}>
       {(provided) => (
         <div key={index}>
           <Paper className={classes.root} {...provided.dragHandleProps}>
             <CssBaseline />
-            <Title title={title} />
+            <Suspense fallback={loading}>
+              <Title title={title} />
+            </Suspense>
             <div {...provided.draggableProps} ref={provided.innerRef}>
               <Droppable droppableId={id}>
                 {(provided) => (
@@ -38,14 +42,18 @@ export default function List({ list, index }: ListProps): ReactElement {
                     className={classes.cardContainer}
                   >
                     {cards.map((card, index) => (
-                      <Card key={card.id} card={card} index={index} />
+                      <Suspense fallback={loading}>
+                        <Card key={card.id} card={card} index={index} />
+                      </Suspense>
                     ))}
                     {provided.placeholder}
                   </div>
                 )}
               </Droppable>
             </div>
-            <InputContainer listId={id} type="card" />
+            <Suspense fallback={loading}>
+              <InputContainer listId={id} type="card" />
+            </Suspense>
           </Paper>
         </div>
       )}
